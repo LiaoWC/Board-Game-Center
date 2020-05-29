0674af6cfda18e90d6419024510a0b19a8d590c3
Brianiiix <brian890309@gmail.com>
2020-05-29 12:39:13 +0800

    funtionality query

---
+++ query/range.sql
@@ -0,0 +1,14 @@
+//range
+
+minplayer=2 maxplayer=5 
+min_playtime=30 max_playtime=60
+board_category="Card Game"
+
+select *
+from info
+where (max_player<=5 & max_player>=2)|(min_player<=5 & min_player>=2)|(max_player>=5 & min_player<=2)
+and (max_playtime<=60 & max_playtime>=30)|(min_playtime<=60 & min_playtime>=30)|(max_playtime>=60 & min_playtime<=30)
+and board_category like "%Card Game%";
+
+
+
---
+++ query/不同種類遊戲的出版數量對上時間.sql
@@ -0,0 +1,5 @@
+- 不同種類遊戲的出版數量對上時間的折線圖
+//info
+id, name, year_published, board_category
+, max_player, min_player, max_playtime, min_playtime
+, age, rating, rating_player
