82ad3e2ef6c874e1f549be6a724309e8350ee5cb
Brianiiix <brian890309@gmail.com>
2020-05-30 16:53:23 +0800

    query

--- query/range.sql
+++ query/range.sql
@@ -1,5 +1,6 @@
 //range
 
+//15 specific category(can change variable)
 minplayer=2 maxplayer=5 
 min_playtime=30 max_playtime=60
 board_category="Card Game"
@@ -10,5 +11,6 @@ where (max_player<=5 & max_player>=2)|(min_player<=5 & min_player>=2)|(max_playe
 and (max_playtime<=60 & max_playtime>=30)|(min_playtime<=60 & min_playtime>=30)|(max_playtime>=60 & min_playtime<=30)
 and board_category like "%Card Game%";
 
+//others
 
 
