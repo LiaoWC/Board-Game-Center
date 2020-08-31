22b43d3a7dfcb97f68896e4db5406f605fefa5a0
liaoWC <xaviliaoweichen@gmail.com>
2020-09-01 02:47:06 +0800

    1.把程式從虛擬環境中分離移至新的資料夾，並移除虛擬環境。

--- venv/practice/1.py
+++
@@ -1,5 +0,0 @@
-print("a'b")
-print("%")
-str = " and board_category not like \'%Card Game%\' and board_category not like \'%Wargame%\' and board_category not like \'%Children\'s Game%\' and board_category not like \'%Dice%\' and board_category not like \'%Fantasy%\' and board_category not like \'%Abstract Strategy%\' and board_category not like \'%Miniatures%\' and board_category not like \'%Educational%\' and board_category not like \'%Party Game%\' and board_category not like \'%Science Fiction%\' and board_category not like \'%Fighting%\' and board_category not like \'%Trivia%\' and board_category not like \'%Economic%\' and board_category not like \'%Movies / TV / Radio theme%\' and board_category not like \'%Print & Play%\'"
-
-print(str)
